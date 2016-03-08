5b09031a6a87ed586bad198a344db9781480eb98
nickzuck <nikhil1945.singh@gmail.com>
2016-03-08 21:03:34 +0530

    Saving the data perfectl

--- getRanks.py
+++ getRanks.py
@@ -7,7 +7,7 @@ class Spoj:
     
     def __init__(self, user_names):
         self.defaultUrl = "http://spoj.com/users/"
-        self.data = [] # To store html data for each user 
+        self.data = {} # To store html data for each user 
         for name in user_names:
             print "Fetching data for %s ....." %(name)
             self.fetchData(name)
@@ -15,10 +15,27 @@ class Spoj:
 
     def fetchData(self, name):
         temp = urllib2.urlopen(self.defaultUrl + str(name))
-        self.data.append({name: temp})
+        print temp 
+        self.data[name] = temp
         print "Data retrived for %s" %(name)
-        soup = BeautifulSoup(temp, "html.parser")
+        # soup = BeautifulSoup(temp, "html.parser")
+        # print soup
+        self.getRanks(name)
+
+    def getRanks(self, name):
+        data = self.data 
+        searched_word ="World Rank"
+        soup = BeautifulSoup(data[name], "html.parser")
         print soup
+        
+
+    def fetchRank(self, html):
+        word = "World Rank"
+        # results = soup.body.find_all(string = re.compile)
+            
+
+    
+
 
 
 if __name__ == '__main__':
