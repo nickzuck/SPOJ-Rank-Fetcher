5b3b0b1a1d079c423a2fd7dd7866323456bccd02
nickzuck <nikhil1945.singh@gmail.com>
2016-03-08 21:20:03 +0530

    Priting the data perfectly ...Need to add little comments to the code to make it readable

--- getRanks.py
+++ getRanks.py
@@ -8,32 +8,41 @@ class Spoj:
     def __init__(self, user_names):
         self.defaultUrl = "http://spoj.com/users/"
         self.data = {} # To store html data for each user 
+        self.results = {}
+        self.users = user_names
         for name in user_names:
             print "Fetching data for %s ....." %(name)
             self.fetchData(name)
+        print "**********All data retrived********** \n\n"
 
 
     def fetchData(self, name):
         temp = urllib2.urlopen(self.defaultUrl + str(name))
-        print temp 
+        # print temp 
         self.data[name] = temp
         print "Data retrived for %s" %(name)
         # soup = BeautifulSoup(temp, "html.parser")
         # print soup
-        self.getRanks(name)
+        self.getRank(name)
 
-    def getRanks(self, name):
+    def getRank(self, name):
         data = self.data 
         searched_word ="World Rank"
         soup = BeautifulSoup(data[name], "html.parser")
-        #print soup
+        # print soup
         results = soup.body.find_all(string=re.compile('.*{0}.*'.format(searched_word)), recursive=True)
-        print results
+        # print results
         for content in results : 
-            print content
+            self.results[name] = content.strip()
+
+
+    def printRanks(self):
+        for i in self.users:
+            print "----------Rank of %s----------" %(i)
+            print self.results[i]
         
 
 
 if __name__ == '__main__':
     obj = Spoj(map(str, raw_input().split(" ")))
-    
+    obj.printRanks()
